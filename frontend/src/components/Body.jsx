654916d979874069588b7e5bb9369278e8947365
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2025-04-10 14:11:11 +0530

    importing font

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -10,9 +10,6 @@ const Body = () => {
         marginTop: '92px', // Adjust this value to match the height of your Navbar
       }}
     >
-      <p className="text-lg text-gray-800 leading-relaxed px-4">
-      We care about creating beautiful architecture, developing projects that are individual, inspiring, and enhancing the lifestyle of users.
-      </p>
     </div>
   );
 };
