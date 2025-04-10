f7478c28c67d784387e9c4ae0d22d64b81d9cd3f
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2025-04-10 11:45:47 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+//Middleware
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(5000,()=>{
+  console.log("listening on server 5000");
+})
