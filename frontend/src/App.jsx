379742afee6b81e27a6274483b302ce66f58abbc
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2025-05-04 16:53:52 +0530

    Organizing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Body from './components/Body';
-import { SwipeCarousel } from './components/SwipeCarousel';
-import AboutUs from './components/AboutUs';
-import ProjectPage from './components/ProjectsPage';
-import ProjectDetail from './components/ProjectDetail';
+import Navbar from './components/User/Navbar';
+import Body from './components/User/Body';
+import { SwipeCarousel } from './components/User/SwipeCarousel';
+import AboutUs from './components/User/AboutUs';
+import ProjectPage from './components/User/ProjectsPage';
+import ProjectDetail from './components/User/ProjectDetail';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
